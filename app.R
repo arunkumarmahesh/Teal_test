d8c1b43df0e627a09ec6fed710e1207699dc802a
vedhav <vedhaviyash4@gmail.com>
2024-01-08 12:35:29 +0530

    fix the teal_data module and shiny server logic

--- app.R
+++ app.R
@@ -1,86 +1,69 @@
-library(htmltools)
-library(rlang)#
-library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
-library(DT)
-library(admiral)
-library(checkmate)
-library(formatters)
-library(rtables)
-library(tern)#
-library(teal.widgets) 
-library(teal.logger) 
-library(teal.code) 
-library(teal.data)
-library(teal.slice)
-library(teal.transform)
 library(teal)
-library(teal.modules.clinical)#
-library(dplyr)
-library(nestcolor)
-library(ggplot2)
-library(tibble)
+library(readxl)
 
-Year_list <- system(paste0("ls ~/Desktop/DEA_APP/test"), intern = T)
-  
+root_path <- "test"
+
 data_mod <- teal_data_module(
   ui = function(id) {
-    ns = NS(id) 
+    ns <- NS(id)
     list(
-  selectizeInput(
-    inputId = ns("Year"),
-    label   = 'Select Year',
-    choices = Year_list),
-  
-  selectizeInput(
-    inputId = ns("Month"),
-    label   = 'Select Month',
-    choices = NULL),
-  
-  selectizeInput(
-    inputId = ns("Fruits"),
-    label   = 'Select Fruits',
-    choices = NULL),
-    actionButton(ns("submit"), "Submit"))
+      selectizeInput(
+        inputId = ns("Year"),
+        label   = "Select Year",
+        choices = list.files(root_path)
+      ),
+      selectizeInput(
+        inputId = ns("Month"),
+        label   = "Select Month",
+        choices = NULL
+      ),
+      selectizeInput(
+        inputId = ns("Fruits"),
+        label   = "Select Fruits",
+        choices = NULL
+      ),
+      actionButton(ns("submit"), "Submit")
+    )
   },
-  
   server = function(id) {
     moduleServer(id, function(input, output, session) {
-      
-      Month <- reactive({
-        req(input$Year)
+      observeEvent(input$Year, {
+        updateSelectInput(
+          inputId = "Month",
+          choices = list.files(path = file.path(
+            root_path,
+            input$Year
+          ))
+        )
       })
-      observeEvent(Month(),{
-        updateSelectInput(inputId = "Month",
-                          choices = list.files(path = file.path("~/Desktop/DEA_APP/test",
-                                                                input$Year)))
+
+      observeEvent(input$Month, {
+        updateSelectInput(
+          inputId = "Fruits",
+          choices = list.files(path = file.path(
+            root_path,
+            input$Year, input$Month
+          ))
+        )
       })
-      
-        Fruits <- reactive({
-          req(input$Month)
-        })
-        observeEvent(Fruits(),{
-          updateSelectInput(inputId = "Fruits",
-                            choices = list.files(path = file.path("~/Desktop/DEA_APP/test",
-                                                                  input$Year, input$Month)))
-        })
-        
-        dat1 <- reactive({
-          req(input$Fruits,input$Month, input$Year)
-          readxl::read_excel(input$Year, input$Month, input$Fruits)
-        })
-        
-        
-        eventReactive(input$submit, {
-          dat1 <- within(
-            teal_data(),{
-              d1 <- dat1()
-              
-            })
-          datanames(dat1) <- "d1"
-          dat1
-        })
+
+      dat1 <- reactive({
+        req(input$Fruits, input$Month, input$Year)
+        readxl::read_excel(file.path(root_path, input$Year, input$Month, input$Fruits))
+      })
+
+
+      eventReactive(input$submit, {
+        dat1 <- within(
+          teal_data(),
+          {
+            d1 <- selected_data
+          },
+          selected_data = dat1()
+        )
+        datanames(dat1) <- "d1"
+        dat1
+      })
     })
   }
 )
